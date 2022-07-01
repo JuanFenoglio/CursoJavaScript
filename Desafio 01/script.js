dda53515895637d89b83c38dbe1b35bac94fcca7
Juan Fenoglio <jj.fenoglio@gmail.com>
2022-07-01 00:24:05 -0300

    correccion de ingreso de datos

--- Desafio 01/script.js
+++ Desafio 01/script.js
@@ -1,11 +1,38 @@
-//Este programa sirve para que un docente ingrese 4 notas de un alumno. 
-//La menor de ellas no sea tenida en cuenta.
-//Se calcule el promedio final del alumno y su condicion de APROBADO o REPROBADO.
+//Este programa sirve para que un docente ingrese 3 notas de un alumno. 
+//Se calcule el promedio final del alumno e informe la condicion de APROBADO o REPROBADO.
 
-//Se definen variables
+console.log("El docente podra introducir 3 notas del alumno por teclado. Con dicha informacion se calculara el promedio final y la condicion de regularidad del alumno");
 
-//Comprobacion de valores ingresados
+//Se definen variables & Comprobacion de valores ingresados
 
-//Calculos
+let nota1 = parseInt(prompt("Ingrese la Nota 01. Recuerde que debe ser entre 0 y 10"));
+while ((nota1 < 0) || (nota1 > 10)) {
+    console.log("La nota ingresada es INVALIDA")
+    nota1 = parseInt(prompt("Ingrese la Nota 01. Recuerde que debe ser entre 0 y 10"));
+}
+
+let nota2 = parseInt(prompt("Ingrese la Nota 02. Recuerde que debe ser entre 0 y 10"));
+while ((nota2 < 0) || (nota2 > 10)) {
+    console.log("La nota ingresada es INVALIDA")
+    nota2 = parseInt(prompt("Ingrese la Nota 02. Recuerde que debe ser entre 0 y 10"));
+}
+
+let nota3 = parseInt(prompt("Ingrese la Nota 03. Recuerde que debe ser entre 0 y 10"));
+while ((nota3 < 0) || (nota3 > 10)) {
+    console.log("La nota ingresada es INVALIDA")
+    nota3 = parseInt(prompt("Ingrese la Nota 03. Recuerde que debe ser entre 0 y 10"));
+}
+
+let regularidad = "DESAPROBADO" // Inicializacion de variable.
+
+
+//Calculos basicos
+
+let promedio = (nota1 + nota2 + nota3) / 3
+if (promedio >= 6) {
+    regularidad = "APROBADO"
+}
 
 //Resultados por pantalla
+
+alert("El promedio del alumno es: " + promedio + " y la condicion final de regularidad es: " + regularidad);
