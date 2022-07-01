e839619a92a093308f82b86db2f214e3e514978d
Juan Fenoglio <jj.fenoglio@gmail.com>
2022-06-30 23:11:11 -0300

    desafio 01 - curso javascript

---
+++ Desafio 01/script.js
@@ -0,0 +1,11 @@
+//Este programa sirve para que un docente ingrese 4 notas de un alumno. 
+//La menor de ellas no sea tenida en cuenta.
+//Se calcule el promedio final del alumno y su condicion de APROBADO o REPROBADO.
+
+//Se definen variables
+
+//Comprobacion de valores ingresados
+
+//Calculos
+
+//Resultados por pantalla
