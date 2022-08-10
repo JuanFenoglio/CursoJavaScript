75c414bcf1f586afeff68b82f8da3f4a4f740225
Juan Fenoglio <jj.fenoglio@gmail.com>
2022-08-09 22:59:48 -0300

    Uso de librearias en proyecto de JS

---
+++ Desafio Final - 3ra Entrega/js/script.js
@@ -0,0 +1,34 @@
+/* SUGD: Sistema Unico de Gestion Docente
+
+Este sitio esta pensando para acercar la educacion secundaria a las nuevas tecnologias y que se genere una sinergia entre INSTITUCION - DOCENTES - ALUMNOS
+
+En la 1ra etapa del sitio se tiene como objetivo:
+A.- Docente carga notas e informacion adicional de sus alumnos y se emite un Informe academico.
+
+En la 2da etapa del sitio se tiene como objetivo:
+A.- Docente toma asistencia de su curso
+B.- Una vez que la etapa A se haya implementada de manera robusta, cambio a asistencia por QR.
+
+En la 3ra etapa del sitio se tiene como objetivo:
+A.- Carga de actividades a cada alumno
+B.- Control de frecuencia de visitas al sitio
+C.- Metricas para docente e institucion
+
+*/
+
+/*
+Control de Notas >>> VALIDACION
+
+1 a 5 > Desaprobado.
+6 o 7 > Aprobado: Bien.
+8 o 9 > Aprobado: Muy bien.
+10 > Aprobado: Excelente.
+
+Control de Condicion Final >>> VALIDACION
+
+1 a 5: Libre.
+6 a 8: Regular.
+9 o 10: Promocionado.
+
+*/
+
